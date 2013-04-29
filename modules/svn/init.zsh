f5f1465c285714d2a4dd3ad2e50d36b45bda8a39
Sanghyun Lee <hammerha@gmail.com>
2013-04-29 13:24:01 +0900

    Add unalias sl

--- modules/svn/init.zsh
+++ modules/svn/init.zsh
@@ -3,6 +3,8 @@ alias sa='svn st | grep "^?" | awk '\''{print $2}'\'' | xargs svn add' # Add new
 alias st='s st'
 alias sd='s diff'
 
+# Remove the prezto default `sl` alias for `ls`
+unalias sl
 sl() {
 	svn log -vl ${1:-5}
 }
