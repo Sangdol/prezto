50041d435fdd5c1b288b89dca59fa8a9e2c290c4
Sangdol <hammerha@gmail.com>
2014-05-13 17:10:52 +0900

    Add fasd vim alias

--- modules/misc/init.zsh
+++ modules/misc/init.zsh
@@ -23,6 +23,7 @@ alias sd='fasd -sid'     # interactive directory selection
 alias sf='fasd -sif'     # interactive file selection
 alias z='fasd_cd -d'     # cd, same functionality as j in autojump
 alias zz='fasd_cd -d -i' # cd with interactive selection
+alias v='f -e vim' # quick opening files with vim
 
 #
 # Shortcuts
