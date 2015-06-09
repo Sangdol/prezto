5728ed92c93107a59d4a6b7529d923ac4d11a4c2
Sangdol <hammerha@gmail.com>
2015-06-09 19:50:02 +0900

    Remove unused fasd aliases

--- modules/misc/init.zsh
+++ modules/misc/init.zsh
@@ -15,14 +15,7 @@ alias zp="cd ~/.zprezto"
 #
 # Fasd (https://github.com/clvv/fasd)
 #
-alias a='fasd -a'        # any
-alias s='fasd -si'       # show / search / select
-alias d='fasd -d'        # directory
-alias f='fasd -f'        # file
-alias sd='fasd -sid'     # interactive directory selection
-alias sf='fasd -sif'     # interactive file selection
 alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-alias zz='fasd_cd -d -i' # cd with interactive selection
 alias v='f -e vim' # quick opening files with vim
 
 #
