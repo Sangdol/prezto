271ff6f69ce16ac58f22788bded744ef2ea25b28
Sanghyun Lee <hammerha@gmail.com>
2017-06-23 12:31:46 +0200

    Tweak code to prevent not putting reason mistake

--- scripts/ppiu.sh
+++ scripts/ppiu.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-STACK_NAME=$1
-REASON=$2
+REASON=$1
+STACK_NAME=$2
+VERSION=$3
 
 if [[ -z $STACK_NAME || -z $REASON ]];then
-  echo "Usage: $0 <stack name> '<reason>'"
+  echo "Usage: $0 '<reason>' <stack name> <version (optional)>"
   exit 0
 fi
 
-#zaws
-LINE=$(senza inst | grep $STACK_NAME)
+LINES=$(senza inst | grep $STACK_NAME)
+
+if [[ "$VERSION" ]]; then
+  LINES=$(echo -e "$LINES" | awk '$2 == '$VERSION' { print $0 }')
+fi
+
+LINE=$(echo -e "$LINES" | head -1)
 REAL_STACK_NAME=$(echo $LINE | awk '{ print $1; }')
 IP=$(echo $LINE | awk '{ print $5; }')
+
+if [[ -z $IP ]]; then
+  echo "No matching instance"
+  exit 1
+fi
 
 echo "Connecting to $REAL_STACK_NAME ($IP) ..."
 
