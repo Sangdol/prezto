7eaa6ef5e3ac177517cc69b9b10dca29747866f2
Sanghyun Lee <hammerha@gmail.com>
2019-12-03 10:47:35 +0100

    Add autoreload config

--- runcoms/ipython/profile_default/startup/00-imports.py
+++ runcoms/ipython/profile_default/startup/00-imports.py
@@ -1,3 +1,11 @@
+%load_ext autoreload
+%autoreload 2
+print()
+print('================')
+print('autoload enabled')
+print('================')
+print()
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
