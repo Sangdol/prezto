9dfa3a8e003b371ca73179ca9a788b1da6fd12f3
Hugh Lee <hammerha@gmail.com>
2020-01-01 15:52:57 +0100

    Improve bluetooth script

--- scripts/connect-to-logitech-bt-adapter.sh
+++ scripts/connect-to-logitech-bt-adapter.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 active_app=$(osascript <<EOD
 tell application "System Events"
 	set activeApp to name of first application process whose frontmost is true
@@ -7,8 +7,19 @@ end tell
 EOD
 )
 
-echo $active_app
+LOGITECH_ID=fc-58-fa-79-f6-88
+
+if [[ $(blueutil --is-connected $LOGITECH_ID) -eq 1 ]];then
+  echo "Already connected."
+  exit 0
+fi
+
+echo "Current app: $active_app"
+
 if [[ "$active_app" != "Melodics" ]];then
-  BluetoothConnector --connect fc-58-fa-79-f6-88 || true
+  blueutil --connect $LOGITECH_ID
   echo "Connected!"
+else
+  blueutil -p 0
+  echo "Disconnected!"
 fi
