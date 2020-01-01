ff7c76bcae04bdecc68ba2d5085527de726ae3b3
Hugh Lee <hammerha@gmail.com>
2020-01-01 18:17:09 +0100

    Fix bluetooth script

--- scripts/connect-to-logitech-bt-adapter.sh
+++ scripts/connect-to-logitech-bt-adapter.sh
@@ -9,16 +9,15 @@ EOD
 
 LOGITECH_ID=fc-58-fa-79-f6-88
 
-if [[ $(blueutil --is-connected $LOGITECH_ID) -eq 1 ]];then
-  echo "Already connected."
-  exit 0
-fi
-
 echo "Current app: $active_app"
 
 if [[ "$active_app" != "Melodics" ]];then
-  blueutil --connect $LOGITECH_ID
-  echo "Connected!"
+  if [[ $(blueutil --is-connected $LOGITECH_ID) -eq 1 ]];then
+    echo "Already connected."
+  else
+    blueutil --connect $LOGITECH_ID
+    echo "Connected!"
+  fi
 else
   blueutil -p 0
   echo "Disconnected!"
