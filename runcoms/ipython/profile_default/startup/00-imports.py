52df96aa6076819fe25ed40d1840be61aa042df6
Hugh Lee <hammerha@gmail.com>
2020-02-29 22:11:08 +0100

    Add clip() function for ipython

--- runcoms/ipython/profile_default/startup/00-imports.py
+++ runcoms/ipython/profile_default/startup/00-imports.py
@@ -9,3 +9,9 @@ print()
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Copy to clipboard
+# inspired by https://blog.michaelyin.info/send-content-to-clipboard-in-ipython/
+def clip(var):
+    import os
+    os.system(f'printf "{var}" | pbcopy')
