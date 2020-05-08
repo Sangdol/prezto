c9520092f61361030dfd5eb03ec2bc1faac8be22
Hugh Lee <hammerha@gmail.com>
2020-05-08 22:12:29 +0200

    Add lein-doo

--- config/profiles.clj
+++ config/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
+{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
+                  [lein-doo "0.1.10"]]
         :dependencies [[pjstadig/humane-test-output "0.9.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
