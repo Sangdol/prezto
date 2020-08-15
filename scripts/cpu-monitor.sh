339e52404d78ac8c113955b74e20be32da17726a
Sanghyun Lee <hammerha@gmail.com>
2020-08-15 22:12:00 +0200

    Update cpu monitor script

--- scripts/cpu-monitor.sh
+++ scripts/cpu-monitor.sh
@@ -2,7 +2,7 @@
 
 # https://stackoverflow.com/questions/24129903/notifying-when-using-high-cpu-via-applescript-or-automator
 # -l 10: sampling for 10 seconds
-read -r pct name < <(/usr/bin/top -l 10 -n 1 -F -o cpu -stats cpu,command | tail -1)
-if (( ${pct%.*} >= 50 )); then
-  terminal-notifier -message "Process > 50%: $name ($pct%)"
+read -r pct name < <(/usr/bin/top -l 60 -n 1 -F -o cpu -stats cpu,command | tail -1)
+if (( ${pct%.*} >= 80 )); then
+  terminal-notifier -message "Process > 80%: $name ($pct%)"
 fi
