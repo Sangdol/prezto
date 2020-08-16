11556eed6dd73bcbcd2c40dbbf07a821b2a9764c
Sanghyun Lee <hammerha@gmail.com>
2020-08-16 23:44:17 +0200

    cpu monitor: set notification timeout and log

--- scripts/cpu-monitor.sh
+++ scripts/cpu-monitor.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # https://stackoverflow.com/questions/24129903/notifying-when-using-high-cpu-via-applescript-or-automator
-# -l 10: sampling for 10 seconds
-read -r pct name < <(/usr/bin/top -l 60 -n 1 -F -o cpu -stats cpu,command | tail -1)
-if (( ${pct%.*} >= 80 )); then
-  terminal-notifier -message "Process > 80%: $name ($pct%)"
+SAMPLE_SECONDS=30
+CPU_THRESHOLD=80
+
+read -r pct name < <(/usr/bin/top -l $SAMPLE_SECONDS -n 1 -F -o cpu -stats cpu,command | tail -1)
+if (( ${pct%.*} >= CPU_THRESHOLD )); then
+  msg="Process > 80%: $name ($pct%)"
+  terminal-notifier -message "$msg" -timeout 5
+  echo "$(date +"%Y-%m-%d %H:%M") $msg" >> "$HOME/logs/cpu-monitor.log"
 fi
