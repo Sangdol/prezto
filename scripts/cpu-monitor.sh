e3758a9d8ff6cdd2228a1d0c81ae96f2e9d675ca
Sanghyun Lee <hammerha@gmail.com>
2020-08-19 23:44:03 +0200

    Use average to monitor cpu

--- scripts/cpu-monitor.sh
+++ scripts/cpu-monitor.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
+#
+# Check cpu usage over time and see if the average is higher than threshold.
+#
 
 # https://stackoverflow.com/questions/24129903/notifying-when-using-high-cpu-via-applescript-or-automator
-SAMPLE_SECONDS=30
-CPU_THRESHOLD=80
+SAMPLE_SIZE=15
+DELAY_SECS=3
+CPU_THRESHOLD=50
+TOP_APP_NUM=5
 
-read -r pct name < <(/usr/bin/top -l $SAMPLE_SECONDS -n 1 -F -o cpu -stats cpu,command | tail -1)
+# -l: samples
+# -s: deplay-secs
+# -n: number of processes
+# -F: Do not calculate statistics on shared libraries, also known as frameworks.
+# -o: order by (desc)
+# -stats: display keys
+read -r name pct < <(/usr/bin/top -s $DELAY_SECS -l $SAMPLE_SIZE -n $TOP_APP_NUM -F -o cpu -stats cpu,pid,command | \
+  grep -A$TOP_APP_NUM "%CPU" | \
+  grep -v CPU | \
+  grep -v "^--" | \
+  awk '{arr[$2]+=$1;arr2[$2]=$3} END {for (key in arr) printf("%s\t%s\n", arr2[key], arr[key])}' | \
+  sort -n -k2 | \
+  tail -1)
+
+pct=$(bc -l <<< "scale=1;$pct / $SAMPLE_SIZE")
+
+# debugging
+# echo "$name $pct"
+
 if (( ${pct%.*} >= CPU_THRESHOLD )); then
-  msg="Process > 80%: $name ($pct%)"
+  msg="Process > $CPU_THRESHOLD%: $name ($pct%)"
   terminal-notifier -message "$msg" -timeout 5
   echo "$(date +"%Y-%m-%d %H:%M") $msg" >> "$HOME/logs/cpu-monitor.log"
 fi
