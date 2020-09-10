4118887046b372560f6f441cf925a76e288d888e
Sanghyun Lee <hammerha@gmail.com>
2020-09-10 11:56:59 +0200

    updage bluetooth script

--- scripts/bluetooth.sh
+++ scripts/bluetooth.sh
@@ -8,16 +8,18 @@ EOD
 )
 
 LOGITECH_ID=fc-58-fa-79-f6-88
+ACT_ON=04-fe-a1-6a-96-fe
+CURRENT_DEVICE=$ACT_ON
 
 echo "Current app: $active_app"
 
 if [[ "$active_app" != "Melodics" ]];then
-  if [[ $(blueutil --is-connected $LOGITECH_ID) -eq 1 ]];then
+  if [[ $(blueutil --is-connected $CURRENT_DEVICE) -eq 1 ]];then
     echo "Already connected."
   else
     blueutil -p 1
     sleep 2  # wait until it turns on bluetooth
-    blueutil --connect $LOGITECH_ID && echo "Connected!"
+    blueutil --connect $CURRENT_DEVICE && echo "Connected!"
   fi
 else
   blueutil -p 0 && echo "Disconnected!"
